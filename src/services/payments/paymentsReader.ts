10daaa7b196b9ce01b21784edfff6fe90c9bf57a
Guimarães Mahota <guimaraesmahota@gmail.com>
2021-08-24 23:17:01 +0200

    add payment pedding list

--- src/services/payments/paymentsReader.ts
+++ src/services/payments/paymentsReader.ts
@@ -12,7 +12,8 @@ const readFiles = async () => {
         host: process.env.FTP_PServices_Host,
         user: process.env.FTP_PServices_User,
         password: process.env.FTP_PServices_Password,
-        port: Number(process.env.FTP_PServices_Port) || 21
+        port: Number(process.env.FTP_PServices_Port) || 21,
+
     }).then(async (serverMessage) => {
         console.log('Server message: ' + serverMessage);
 
@@ -21,23 +22,19 @@ const readFiles = async () => {
         await list
             .filter((item: any) => item["type"] === "-")
             .forEach(async (item: any) => {
+                ftp.cwd('/To Import');
 
-                console.log(item)
+                var stream = await ftp.get(`${item.name}`)
+                const chunks: any = [];
 
-                await ftp.get(`/To Import/${item.name}`, true)
-                    .then((stream) => {
-                        console.log(stream)
-                        getStream(stream).then(r => {
-                            let item: ServicePayment = convertToServicePayment(String(r))
+                for await (let chunk of stream) {
+                    chunks.push(chunk)
+                }
 
-                            lists.push(item)
-                        });
-                    }).catch((err) => {
-                        console.log(err)
-                    })
+                const buffer = Buffer.concat(chunks);
+                const str = buffer.toString("utf-8")
 
-
-                // convert stream to string
+                lists.push(convertToServicePayment(String(str)))
 
             })
 
@@ -47,19 +44,7 @@ const readFiles = async () => {
         return ftp.end();
     });
 
-
     return lists
-
-    function getStream(stream: any) {
-        return new Promise(resolve => {
-            const chunks: any = [];
-
-            //Buffer.from is required if chunk is a String, see comments
-            stream.on("data", (chunk: any) => chunks.push(Buffer.from(chunk)));
-            stream.on("end", () => resolve(Buffer.concat(chunks).toString()));
-        });
-    }
-
 
     function convertToServicePayment(text: string) {
         try {
@@ -71,17 +56,18 @@ const readFiles = async () => {
                 items: []
             }
             let items: ServicePaymentLine[] = []
+
             const lines = text.split(/(?:\r\n|\r|\n)/g)
             lines
-                .filter((line: string) => line.substring(0, 1) === "2")
+                .filter((line: string) => line.substr(0, 1) === "2")
                 .forEach((line: string) => {
                     const item: ServicePaymentLine = {
                         id: 0,
-                        reference: line.substring(1, 11),
-                        date: convertToDate(line.substring(44, 8), "DDMMyyyy"),
-                        transactionId: line.substring(58, 7),
-                        amount: convertToNumber(line.substring(12, 16), 2),
-                        rate: convertToNumber(line.substring(28, 16), 2),
+                        reference: line.substr(1, 11),
+                        date: convertToDate(line.substr(44, 8), "DDMMyyyy"),
+                        transactionId: line.substr(58, 7),
+                        amount: convertToNumber(line.substr(12, 16), 2),
+                        rate: convertToNumber(line.substr(28, 16), 2),
                     }
 
                     items.push(item)
